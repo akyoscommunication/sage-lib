d1b92571636a6c13c34a110d61dcd009a8d9ec1e
Thomas Sebert <thomas.sebert.akyos@gmail.com>
2025-01-06 14:33:45 +0100

    Update Container

--- Container.php
+++ Container.php
@@ -6,4 +6,16 @@ use Illuminate\Container\Container as BaseContainer;
 
 class Container extends BaseContainer
 {
+    /**
+     * Register a terminating callback with the application.
+     * @param callable|string $callback
+     * @return \Roots\Sage\Container
+     * @throws \Illuminate\Contracts\Container\BindingResolutionException
+     */
+    public function terminating(callable|string $callback): static
+    {
+        $this->make('events')->listen('kernel.terminating', $callback);
+
+        return $this;
+    }
 }
